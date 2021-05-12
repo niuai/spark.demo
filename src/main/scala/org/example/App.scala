081b91ad374efe16e095ae4a239fb6a82639ee54
niuai <1196454838@qq.com>
2021-05-12 17:11:20 +0800

    add scala project

---
+++ src/main/scala/org/example/App.scala
@@ -0,0 +1,20 @@
+package org.example
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.{SparkConf, SparkContext}
+
+object App {
+  def main(args: Array[String]) {
+    Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
+
+    val inputFile = "/opt/hdfs/input.txt"
+
+    val conf = new SparkConf().setAppName("WordCount")//.setMaster("local")
+    val sc = new SparkContext(conf)
+    val textFile = sc.textFile(inputFile)
+    val wordCount = textFile.flatMap(line => line.split(" ")).map(word => (word, 1)).reduceByKey((a, b) => a + b)
+    wordCount.foreach(println)
+
+    sc.stop()
+  }
+}
