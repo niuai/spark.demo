a39e7ee7cfb79a6ee644d968501f3dfd1f0f7ccf
niuai <1196454838@qq.com>
2021-05-13 14:54:06 +0800

    add readme.md file

--- src/main/scala/org/example/App.scala
+++ src/main/scala/org/example/App.scala
@@ -7,11 +7,12 @@ object App {
   def main(args: Array[String]) {
     Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
 
-    val inputFile = "file:///opt/hdfs/input.txt"
+    val inputFile = "src/main/resources/README.md"
 
-    val conf = new SparkConf().setAppName("WordCount")//.setMaster("local")
+    val conf = new SparkConf().setAppName("WordCount").setMaster("local")
     val sc = new SparkContext(conf)
     val textFile = sc.textFile(inputFile)
+
     val wordCount = textFile.flatMap(line => line.split(" ")).map(word => (word, 1)).reduceByKey((a, b) => a + b)
     wordCount.foreach(println)
 
