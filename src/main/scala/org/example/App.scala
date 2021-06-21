8f27df2a221599df1b6b8be80a64896c6b1fd654
niuai <1196454838@qq.com>
2021-06-21 15:08:21 +0800

    add stream

--- src/main/scala/org/example/App.scala
+++ src/main/scala/org/example/App.scala
@@ -1,7 +1,10 @@
 package org.example
 
 import org.apache.log4j.{Level, Logger}
-import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.streaming.dstream.{DStream, ReceiverInputDStream}
+import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 object App {
   def main(args: Array[String]) {
@@ -9,12 +12,21 @@ object App {
 
     val inputFile = args(1)
 
-    val conf = new SparkConf().setAppName("WordCount").setMaster(args(0))
-    val sc = new SparkContext(conf)
+    val conf = new SparkConf().setAppName("App").setMaster(args(0))
+    val session = SparkSession.builder.config(conf).getOrCreate()
+    val sc = session.sparkContext
+    val streamContext = new StreamingContext(sc, Seconds(2))
     val textFile = sc.textFile(inputFile)
 
-    val wordCount = textFile.flatMap(line => line.split(" ")).map(word => (word, 1)).reduceByKey((a, b) => a + b)
-    wordCount.foreach(println)
+    val inDStream: ReceiverInputDStream[String] = streamContext.socketTextStream("127.0.0.1", 9000)
+    inDStream.print()
+
+    val resultDStream: DStream[(String, Int)] = inDStream.flatMap(_.split(",")).map((_, 1)).reduceByKey(_ + _)
+    resultDStream.print()
+
+    streamContext.start()
+    streamContext.awaitTermination()
+    streamContext.stop()
 
     sc.stop()
   }
---
+++ src/main/scala/org/example/People.scala
@@ -0,0 +1,3 @@
+package org.example
+
+case class People(var name: String, var age: Int)
